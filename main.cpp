07f0782ebd6ee2e7d2a3c12f13facc866b28270d
mwijeyasinghebbx <62507591+mwijeyasinghebbx@users.noreply.github.com>
2020-11-19 13:52:55 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {   
-    cout << "Hello World!" << endl;    
+    cout << "Hello World from git deployment!" << endl;    
     cout << "Hello World 2!" << endl;
     return 0;
 }
